640c0190e6478c48d72a75492ccb52b545461753
SNASR29 <150824216+SNASR29@users.noreply.github.com>
2023-12-03 18:14:57 -0500

    Update Main.java

--- src/main/java/Sergio/example/Main.java
+++ src/main/java/Sergio/example/Main.java
@@ -1,19 +1,48 @@
 package Sergio.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import Sergio.example.dto.Department;
+import Sergio.example.dto.SchoolManagementSystem;
+import Sergio.example.dto.Student;
+import Sergio.example.dto.Course;
+import Sergio.example.dto.Teacher;
+
+/**
+ * main class that prints info and adds info
+ * 
+ * @author Sergio Nasr(ID: 2376102)
+ */
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        SchoolManagementSystem s1 = new SchoolManagementSystem();
+        s1.addDepartment("Math");
+        s1.addDepartment("Physics");
+        s1.addDepartment("Biology");
+        s1.addDepartment("Chemistry");
+        s1.addDepartment("English");
+        s1.addDepartment("French");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        s1.printDepartment();
+
+        s1.addTeacher("Yi", "Wang", new Department("Math"));
+        s1.addTeacher("Yi", "Wang", new Department("Math"));
+        s1.addTeacher("Yi", "Wang", new Department("Math"));
+        s1.addTeacher("Yi", "Wang", new Department("Math"));
+        s1.addTeacher("Yi", "Wang", new Department("Math"));
+        s1.addTeacher("Yi", "Wang", new Department("Math"));
+        s1.addTeacher("Yi", "Wang", new Department("Math"));
+
+        s1.printTeachers();
+
+        s1.addStudent("Sergio", "Nasr", new Department("Math"));
+        s1.addStudent("Sergio", "Nasr", new Department("Math"));
+        s1.addStudent("Sergio", "Nasr", new Department("Math"));
+        s1.addStudent("Sergio", "Nasr", new Department("Math"));
+        s1.addStudent("Sergio", "Nasr", new Department("Math"));
+        s1.addStudent("Sergio", "Nasr", new Department("Math"));
+        s1.addStudent("Sergio", "Nasr", new Department("Math"));
+        s1.addStudent("Sergio", "Nasr", new Department("Math"));
+
+        s1.printStudents();
     }
 }

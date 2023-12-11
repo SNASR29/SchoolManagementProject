283f0a0fb172e43972235383421b6b2f9f54b55b
SNASR29 <150824216+SNASR29@users.noreply.github.com>
2023-12-10 23:16:07 -0500

    Update SchoolManagment.java

--- src/main/java/Sergio/example/dto/SchoolManagment.java
+++ src/main/java/Sergio/example/dto/SchoolManagment.java
@@ -12,6 +12,9 @@ import lombok.ToString;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @ToString
 @Getter
 @AllArgsConstructor
@@ -24,97 +27,193 @@ public class SchoolManagementSystem {
     public static final int MAX_TEACHER_NUMBER = 20;
     public static final int MAX_COURSE_NUMBER = 30;
 
-    private Department[] departments = new Department[MAX_DEPARTMENT_NUMBER];
-    private Student[] students = new Student[MAX_STUDENT_NUMBER];
-    private Teacher[] teachers = new Teacher[MAX_TEACHER_NUMBER];
-    private Course[] courses = new Course[MAX_COURSE_NUMBER];
+    private Department[] departments;
+    private Student[] students;
+    private Teacher[] teachers;
+    private Course[] courses;
+    private int departmentCount;
+    private int courseCount;
+    private int studentCount;
+    private int teacherCount;
 
-    private int departmentCount = 0;
-    private int studentCount = 0;
-    private int courseCount = 0;
-    private int teacherCount = 0;
-
-    public SchoolManagementSystem(){}
+    public SchoolManagementSystem() {
+        this.departments = new Department[MAX_DEPARTMENT_NUMBER];
+        this.courses = new Course[MAX_COURSE_NUMBER];
+        this.students = new Student[MAX_STUDENT_NUMBER];
+        this.teachers = new Teacher[MAX_TEACHER_NUMBER];
+        this.departmentCount = 0;
+        this.courseCount = 0;
+        this.teacherCount = 0;
+        this.studentCount = 0;
+    }
 
 
     /**
-     * Method that finds the requested department
+     * Search a department in the departments based on a departmentId. If the departmentId
+     * does not match with anything in the departments, return null.
      * @param departmentId
-     * @return info about the desired department according to the department class format
+     * @return department
      */
     public Department findDepartment(String departmentId) {
-        if (departmentCount < MAX_DEPARTMENT_NUMBER) {
-            Department newDepartment = new Department(departmentId);
-            departments[departmentCount] = newDepartment;
-            departmentCount++;
-            System.out.println("Department added:" + newDepartment);
+        for (int i = 0; i < departmentCount; i++) {
+            Department department = departments[i];
+            if (department != null && department.getDepartmentId().equals(departmentId)) {
+                System.out.println(departments[i]);
+                return department;
+            }
         }
         return null;
     }
 
     /**
+     * Display all teachers in a school, only display the teacher that is not null. And only display
+     * the names of course and department if there is any.
      * Method that prints the names of the teachers
      */
     public void printTeachers() {
+        System.out.println("List of Teachers with Courses:");
         for (int i = 0; i < teacherCount; i++) {
-            System.out.println(teachers[i]);
+            Teacher teacher = teachers[i];
+            if (teacher != null) {
+                System.out.println(teacher.toString());
+
+                Department teacherDepartment = teacher.getDepartment();
+                if (teacherDepartment != null) {
+                    System.out.println("Department: " + teacherDepartment.getName());
+                }
+
+                Course[] teacherCourses = teacher.getCourses();
+                for (Course course : teacherCourses) {
+                    if (course != null) {
+                        System.out.println("Course: " + course.getCourseName());
+                    }
+                }
+                System.out.println();
+            }
         }
     }
 
     /**
-     * Method that changes the teacher for the according course using the id of the teacher and the course
+     * Display all courses in a school, only display the course that is not null. And only display
+     * the names of teacher, students, and department if there is any.
+     */
+    public void printCourses() {
+        System.out.println("List of Courses with Details:");
+        for (int i = 0; i < courseCount; i++) {
+            Course course = courses[i];
+            if (course != null) {
+                System.out.println(course.toString());
+
+                Department courseDepartment = course.getDepartment();
+                if (courseDepartment != null) {
+                    System.out.println("Department: " + courseDepartment.getName());
+                }
+
+                Teacher courseTeacher = course.getTeacher();
+                if (courseTeacher != null) {
+                    System.out.println("Teacher: " + courseTeacher.getFirstName() + " " + courseTeacher.getLastName());
+                }
+
+                Student[] courseStudents = course.getStudents();
+                for (Student student : courseStudents) {
+                    if (student != null) {
+                        System.out.println("Student: " + student.getFirstName() + " " + student.getLastName());
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+
+    /**
+     *Assign a teacher to a specific course, based on teacherId and courseId, if no teacher or
+     * course can be found with those ids, print a message
      * @param teacherId
      * @param courseId
      */
     public void modifyCourseTeacher(String teacherId, String courseId) {
+        Teacher teacher = findTeacher(teacherId);
+        Course course = findCourse(courseId);
 
-
+        if (teacher != null && course != null) {
+            course.setTeacher(teacher);
+            System.out.println("Teacher " + teacher.getFirstName() + " " + teacher.getLastName() +
+                    " assigned to course " + course.getCourseName());
+        } else {
+            System.out.println("Teacher or course not found with the specified IDs.");
+        }
     }
 
     /**
-     * This method adds a department to the school
+     *Add a new department
      * @param departmentName
      */
-    public void addDepartment (String departmentName) {
+    public void addDepartment(String departmentName) {
         if (departmentCount < MAX_DEPARTMENT_NUMBER) {
             Department newDepartment = new Department(departmentName);
             departments[departmentCount] = newDepartment;
             departmentCount++;
         } else {
-            System.out.println("Cannot add more departments. Maximum number of departments reached");
+            System.out.println("Cannot add more departments. Maximum number of departments reached.");
         }
     }
 
 
     /**
-     * This method prints out the info of all the students according to the format of the Student class
+     * Display all students in a school, only display the student that is not null. And only display
+     * the names of courses and department if there is any.
      */
     public void printStudents() {
+        System.out.println("List of Students with Courses:");
         for (int i = 0; i < studentCount; i++) {
-            System.out.println(courses[i]);
+            Student student = students[i];
+            if (student != null) {
+                System.out.println(student.toString());
+
+                Department studentDepartment = student.getDepartment();
+                if (studentDepartment != null) {
+                    System.out.println("Department: " + studentDepartment.getName());
+                }
+
+                Course[] studentCourses = student.getCourses();
+                for (Course course : studentCourses) {
+                    if (course != null) {
+                        System.out.println("Course: " + course.getCourseName());
+                    }
+                }
+                System.out.println();
+            }
         }
     }
 
     /**
-     * This method finds the requested student and gives that student's info
+     * Search a student in the students based on a studentId. If the studentId does not match
+     * with anything in the students, return null.
      * @param studentId
-     * @return the info of the student according to the Student class
+     * @return student
      */
-    public Student findStudent (String studentId) {
-
-
+    public Student findStudent(String studentId) {
+        for (int i = 0; i < studentCount; i++) {
+            Student student = students[i];
+            if (student != null && student.getStudentId().equals(studentId)) {
+                System.out.println(students[i]);
+                return student;
+            }
+        }
         return null;
     }
 
 
     /**
-     * This method ads a new course to the shcool
+     * Add a new course
      * @param courseName
      * @param credit
-     * @param courseID
+     * @param department
      */
-    public void addCourse (String courseName, double credit, String courseID) {
-        if (courseCount < MAX_COURSE_NUMBER) {;
+    public void addCourse(String courseName, double credit, Department department) {
+        if (courseCount < MAX_COURSE_NUMBER) {
+            Course newCourse = new Course(courseName, department, credit);
+            courses[courseCount] = newCourse;
             courseCount++;
         } else {
             System.out.println("Cannot add more courses. Maximum number of courses reached");
@@ -124,20 +223,60 @@ public class SchoolManagementSystem {
 
 
     /**
-     * This method registers students to a certain course
-     * @param courseName
+     *Register a course for a student, based on studentId and courseId, which will check
+     * i. If the studentId or courseId does not match with anything, print a message.
+     * ii. If the student has register max amount courses (5), print a message.
+     * iii. If the course has be registered by the max number of students (5), print a
+     * message.
      * @param courseId
+     * @param studentId
      */
-    public void registerCourse (String courseName, String courseId) {
+    public void registerCourse(String studentId, String courseId) {
+        Student student = findStudent(studentId);
+        Course course = findCourse(courseId);
+
+        if (student != null && course != null) {
+            boolean isAlreadyRegistered = false;
+
+            for (int i = 0; i < student.getCourseNum(); i++) {
+                if (student.getCourses()[i].getCourseId().equals(courseId)) {
+                    isAlreadyRegistered = true;
+                    break;
+                }
+            }
+
+            if (isAlreadyRegistered) {
+                System.out.println("Student " + student.getFirstName() + " " + student.getLastName() +
+                        " is already registered for course " + course.getCourseName());
+                return;
+            }
+
+            if (course.getStudentNum() < Course.MAX_STUDENT_NUMBER) {
+                if (student.getCourseNum() < Student.MAX_COURSE_NUMBER) {
+                    student.registerForCourse(course);
+                    course.registerStudent(student);
+
+                    System.out.println("Student " + student.getFirstName() + " " + student.getLastName() +
+                            " registered for course " + course.getCourseName());
+                } else {
+                    System.out.println("Cannot register for more courses. Maximum number of courses reached for student " + student.getFirstName() + " " + student.getLastName());
+                }
+            } else {
+                System.out.println("Cannot register more students. Maximum number of students reached for course " + course.getCourseName());
+            }
+        } else {
+            System.out.println("Student or course not found with the specified IDs.");
+        }
     }
 
+
     /**
-     * This method ads a new teacher to the school
+     * This method adds a new teacher to the school
      * @param teacherFirstName
      * @param teacherLastName
      * @param department
      */
-    public void addTeacher (String teacherFirstName, String teacherLastName, Department department) {
+    public void addTeacher(String teacherFirstName, String teacherLastName, Department department) {
         if (teacherCount < MAX_TEACHER_NUMBER) {
             Teacher newTeacher = new Teacher(teacherFirstName, teacherLastName, department);
             teachers[teacherCount] = newTeacher;
@@ -148,26 +287,36 @@ public class SchoolManagementSystem {
     }
 
     /**
-     * This method finds the info about a course according to the Course class by receiving the name of the course
-     * @param courseName
-     * @return the info about the course according to the Course class
+     * Search a course in the courses based on a courseId. If the courseId does not match with
+     * anything in the courses, return null.
+     * @param courseId
+     * @return course
      */
-    public Course findCourse(String courseName) {
-
+    public Course findCourse(String courseId) {
+        for (int i = 0; i < courseCount; i++) {
+            Course course = courses[i];
+            if (course != null && course.getCourseId().equals(courseId)) {
+                System.out.println(courses[i]);
+                return course;
+            }
+        }
         return null;
     }
 
     /**
-     * This method prints out all the departments
+     * Display all departments in a school, only display the department that is not null
      */
     public void printDepartment() {
-        for (int i = 0; i < departmentCount; i++) {
-            System.out.println(departments[i]);
+        System.out.println("List of departments:");
+        for (int i = 0; i < MAX_DEPARTMENT_NUMBER; i++) {
+            if (departments[i] != null) {
+                System.out.println(departments[i]);
+            }
         }
     }
 
     /**
-     * this method adds a new student to the school
+     * Add a new Stundent
      * @param studentFirstName
      * @param studentLastName
      * @param department
@@ -184,12 +333,19 @@ public class SchoolManagementSystem {
     }
 
     /**
-     * this method finds a teacher in the school by using their Id
+     *Search a teacher in the teachers based on a teacherId. If the teacherId does not match
+     * with anything in the teachers, return null.
      * @param teacherId
-     * @return the info of the teacher according to the Teacher class
+     * @return teacher
      */
     public Teacher findTeacher(String teacherId) {
-
+        for (int i = 0; i < teacherCount; i++) {
+            Teacher teacher = teachers[i];
+            if (teacher != null && teacher.getTeacherId().equals(teacherId)) {
+                System.out.println(teachers[i]);
+                return teacher;
+            }
+        }
         return null;
     }
 }

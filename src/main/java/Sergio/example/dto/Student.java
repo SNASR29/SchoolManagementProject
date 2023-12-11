fda9aeb60755f478d8374f9e6083ce3c23b971ad
SNASR29 <150824216+SNASR29@users.noreply.github.com>
2023-12-10 23:14:06 -0500

    Update Student.java

--- src/main/java/Sergio/example/dto/Student.java
+++ src/main/java/Sergio/example/dto/Student.java
@@ -6,6 +6,8 @@ import lombok.ToString;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 
+import java.util.Arrays;
+
 @ToString
 @Getter
 @AllArgsConstructor
@@ -15,7 +17,7 @@ import lombok.EqualsAndHashCode;
 //the fields of this class
 public class Student {
     public static int nextId = 1;
-    private static final int MAX_COURSE_NUMBER = 5;
+    public static final int MAX_COURSE_NUMBER = 5;
 
     private String firstName;
     private int courseNum;
@@ -26,9 +28,8 @@ public class Student {
     private String name;
 
     //the method that receives the requested info
-    public Student(String studentId, String firstName, String lastname, Department department) {
+    public Student( String firstName, String lastname, Department department) {
         this.studentId = String.format("S%03d", nextId++);
-
         this.courseNum = 0;
         this.firstName = firstName;
         this.lastName = lastname;
@@ -36,9 +37,34 @@ public class Student {
         this.courses = new Course[MAX_COURSE_NUMBER];
     }
 
+    //Method that registers a student to a course( support method)
+    public void registerForCourse(Course course) {
+        if (courseNum < MAX_COURSE_NUMBER) {
+            courses[courseNum] = course;
+            courseNum++;
+            System.out.println("Student " + firstName + " " + lastName +
+                    " registered for course " + course.getCourseName());
+        } else {
+            System.out.println("Cannot register for more courses. Maximum number of courses reached for student " + firstName + " " + lastName);
+        }
+    }
+
     //toString method that adjusts the format of the string
     @Override
     public String toString() {
-        return "Student{ First name = " + firstName + ", last name = " + lastName + ", course number = " + courseNum + ", department = " + department + ", courses = " + courses + ", stundent ID = " + studentId + "}";
+        String coursesString = "[";
+
+        for (int i = 0; i < courseNum; i++) {
+            coursesString += courses[i].getCourseName();
+            if (i < courseNum - 1) {
+                coursesString += ", ";
+            }
+        }
+
+        coursesString += "]";
+
+        return "Student{ First name = " + firstName + ", last name = " + lastName +
+                ", course number = " + courseNum + ", department = " + department +
+                ", courses = " + coursesString + ", student ID = " + studentId + "}";
     }
 }
